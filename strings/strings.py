2d036661c505c306c1ac8521d5b6fa6d2b90e4e4
IvanSotomayor <ivanes99.ios@gmail.com>
2023-12-20 16:19:27 -0600

    add strings folder and file

---
+++ strings/strings.py
@@ -0,0 +1,48 @@
+#String methods 
+string = "authentic77"
+
+#slicing
+print(string[2:5])
+print(string[:3])
+print(string[2:])
+
+#start to slice from end to the beginning
+print(string[:-2])
+
+#upper case
+print(string.upper())
+
+#lower case
+print(string.lower())
+
+#remove whitespaces
+print(string.strip())
+
+#replace a string to another string
+print(string.replace("authentic","false"))
+
+#split a string by a character
+newstring = "my name is ivan"
+print(newstring.split(" "))
+
+#string concantenation
+print("hello" + "world")
+
+#format strings
+age = 23
+text = "hello I am {} years old"
+print(text.format(age))
+
+#illegal characters in a string
+
+
+
+
+
+
+
+
+
+
+
+
