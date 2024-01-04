16f7e9d1ec009e7af582e3d305c2514844def9a8
IvanSotomayor <ivanes99.ios@gmail.com>
2024-01-04 00:47:46 -0600

    add python operators

---
+++ strings/operators.py
@@ -0,0 +1,51 @@
+""" 
+Python divides the operators in the following groups:
+
+Arithmetic operators
+Assignment operators
+Comparison operators
+Logical operators
+Identity operators
+Membership operators
+Bitwise operators
+
+"""
+
+#Arithmetic Operators
+print("ARITHMETIC OPERATORS")
+print(1 + 1)
+print(1 - 1)
+print(2 * 1)
+print(10 / 5)
+print(10 % 2) #module
+print(2 ** 2) #exponentiation
+
+#assignemt
+print("ASSIGNMENTS OPERATORS")
+variable = 7
+variable += 1
+print(variable)
+variable -= 1
+print(variable)
+variable /= 1
+print(variable)
+variable **= 2
+print(variable)
+variable %= 1
+print(variable)
+
+#comparison
+print("COMPARISON OPERATORS")
+print(1 == 1)
+print(7 > 1)
+print(7 < 1)
+print(7 != 1)
+print(7 >= 1)
+print(7 <= 1)
+
+#logical operators
+print("LOGICAL OPERATORS")
+print(7 >1 and 10 > 1)
+print(7 >1 or 10 > 1)
+print(not(7 >1 or 10 > 1))
+
--- strings/strings.py
+++ strings/strings.py
@@ -2,28 +2,25 @@
 string = "authentic77"
 
 #slicing
-print(string[2:5])
-print(string[:3])
-print(string[2:])
+print(string[2:5])  #the
+print(string[:3])  #aut
+print(string[2:])  #thentic77
 
 #start to slice from end to the beginning
-print(string[:-2])
+print(string[:-2]) #authentic
 
 #upper case
-print(string.upper())
+print(string.upper()) #AUTHENTIC77
 
 #lower case
-print(string.lower())
-
-#remove whitespaces
-print(string.strip())
+print(string.lower()) #authentic77
 
 #replace a string to another string
-print(string.replace("authentic","false"))
+print(string.replace("authentic","false")) #false77
 
 #split a string by a character
 newstring = "my name is ivan"
-print(newstring.split(" "))
+print(newstring.split(" ")) #['my', 'name', 'is', 'ivan']
 
 #string concantenation
 print("hello" + "world")
@@ -33,7 +30,6 @@ age = 23
 text = "hello I am {} years old"
 print(text.format(age))
 
-#illegal characters in a string
 
 
 
